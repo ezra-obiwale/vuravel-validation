45962a3d69cf189f326080f0f89965f3bd716610
Ezra Obiwale <ezra.obiwale@gmail.com>
2022-03-02 22:08:48 +0100

    Updates for vue

--- src/index.js
+++ src/index.js
@@ -1,163 +1,24 @@
-import * as availableRules from './rules'
-import { isObject } from './utils'
+import { customRule, validate, validateData } from "@ezraobiwale/laravel-style-validation"
+import { isObject } from "@ezraobiwale/laravel-style-validation/dist/utils"
 
-const customRules = {}
-
-const getAvailableRuleFunction = (name) => {
-    const studlyName = toStudly(name)
-
-    if (availableRules.hasOwnProperty(studlyName)) {
-        return availableRules[studlyName]
-    }
-
-    const otherStudlyName = `$${studlyName}`
-
-    if (availableRules.hasOwnProperty(otherStudlyName)) {
-        return availableRules[otherStudlyName]
-    }
-
-    return
-}
-
-const getRuleFunction = (name) => {
-    const availableRuleFn = getAvailableRuleFunction(name)
-
-    if (availableRuleFn) {
-        return availableRuleFn
-    }
-
-    if (customRules.hasOwnProperty(name)) {
-        return customRules[name]
-    }
-
-    throw new Error(`Validation rule "${name}" does not exist.`)
-}
-
-const toStudly = (str) => str.replace(/(_|-)+[a-z]/gi, chr => chr[1].toUpperCase())
-
-export const asArray = (rules, messages = {}, data = {}, field = null) => {
-    if (typeof rules !== 'string' && !Array.isArray(rules)) {
-        throw new Error('First parameter must be a string of rules')
-    }
-
-    if (!isObject(data)) {
-        throw new Error('Third parameter must be a data object of field to value')
-    }
-
-    const rulesArray = Array.isArray(rules) ? rules : rules.split('|')
-    const ruleNames = [...rulesArray].map(rule => rule.split(':')[0])
-
-    return rulesArray.map(ruleGroup => {
-        const [name] = ruleGroup.split(':')
-        const message = isObject(messages) ? messages[name] : messages
-
-        return asFunction(ruleGroup, { data, field, message: message, rules: ruleNames })
-    })
-}
-
-export const asFunction = (ruleGroup, options = {}) => {
-    if (typeof ruleGroup !== 'string') {
-        throw new Error('First parameter must be a string with pattern rule:comma,separated,options')
-    }
-
-    if (!isObject(options)) {
-        throw new Error('Second parameter must be a data object of field to value')
-    }
-
-    if (options.hasOwnProperty('data') && !isObject(options.data)) {
-        throw new Error('options.data must be an array')
-    }
-
-    const [name, params = ''] = ruleGroup.split(':')
-    const ruleFunc = getRuleFunction(name)
-    const paramsArray = params.split(',').filter(val => !!val.length)
-
-    return value => ruleFunc(value, { ...options, params: paramsArray })
-}
-
-export const customRule = (name, func, override = false) => {
-    if (getAvailableRuleFunction(name)) {
-        throw new Error(`In-built rule "${name}" already exists`)
-    }
-
-    if (customRules.hasOwnProperty(name) && !override) {
-        throw new Error(`Custom rule "${name}" already exists`)
-    }
-
-    if (typeof func !== 'function') {
-        throw new Error('Second parameter must be a function')
-    }
-
-    customRules[name] = func
-}
-
-export const rulesAsArray = (rules, messages = {}, data = {}) => {
-    if (!isObject(rules)) {
-        throw new Error('First parameter must be a rules object: of field to rules string')
-    }
-
-    if (!isObject(data)) {
-        throw new Error('Third parameter must be a data object of field to value')
-    }
-
-    const arrayRules = {}
-
-    for (let field in rules) {
-        arrayRules[field] = asArray(rules[field], (messages && messages[field]) || {}, data, field)
-    }
-
-    return arrayRules
-}
-
-export const validate = (value, rules, messages = {}, data = {}, field = null) => {
-    if (typeof rules !== 'string' && !Array.isArray(rules)) {
-        throw new Error('Second parameter must a string of rules')
-    }
-
-    if (!isObject(data)) {
-        throw new Error('Fourth parameter must be a data object of field to value')
-    }
-
-    const disposableFieldRules = asArray(rules, messages, data, field)
-    let valid = true
-
-    while (valid && disposableFieldRules.length) {
-        const currentRule = disposableFieldRules.shift()
-
-        valid = currentRule(value)
-    }
-
-    return valid
-}
-
-export const validateData = (data, rules, messages = {}) => {
-    if (!isObject(data)) {
-        throw new Error('First parameter must be a data object of field to value')
-    }
-
-    if (!isObject(rules)) {
-        throw new Error('Second parameter must be a rules object of field to rules string')
-    }
-
-    const result = {}
-
-    for (let field in data) {
-        let fieldRules = rules[field]
-
-        if (!fieldRules) {
-            continue
+export default {
+    install (app, customRules = {}) {
+        if (!isObject(customRules)) {
+            throw new Error('Options must be an object of custom rules')
         }
 
-        let fieldMessages = {}
-
-        if (messages === false) {
-            fieldMessages = false
-        } else if (messages.hasOwnProperty(field)) {
-            fieldMessages = messages[field]
+        for (let name in customRules) {
+            customRule(name, customRules[name])
         }
 
-        result[field] = validate(data[field], fieldRules, fieldMessages, data, field)
+        if (app.config?.globalProperties) {
+            app.config.globalProperties.$customRule = customRule
+            app.config.globalProperties.$validate = validate
+            app.config.globalProperties.$validateData = validateData
+        } else {
+            app.$customRule = $customRule
+            app.$validate = validate
+            app.$validateData = validateData
+        }
     }
-
-    return result
 }
